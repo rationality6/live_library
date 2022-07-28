4ed782632f81769fefc050dd48af8ca5121f1eca
Hyun Ahn <codepawn@icloud.com>
2022-07-28 18:08:32 +0900

    added seeds datas

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,21 +1,33 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Library.Repo.insert!(%Library.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
-
 books = [
-  %Library.Book{name: "12가지 인생의 법칙", author: "조던 피터슨"},
-  %Library.Book{name: "의미의 지도", author: "조던 피터슨"},
-  %Library.Book{name: "영혼의 지도", author: "융"},
-  %Library.Book{name: "죄와 벌", author: "도스토예프스키"}
+  %Library.Book{
+    name: "12 Rules for Life",
+    author: "Jordan B. Peterson",
+    image: "https://m.media-amazon.com/images/I/41bK7Jt8k+L.jpg"
+  },
+  %Library.Book{
+    name: "Maps of Meaning",
+    author: "Jordan B. Peterson",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/41o3KPXTjYL._SX364_BO1,204,203,200_.jpg"
+  },
+  %Library.Book{
+    name: "Map of the Soul",
+    author: "Jung",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/41BJmkr9M7L._SX332_BO1,204,203,200_.jpg"
+  },
+  %Library.Book{
+    name: "The brothers karamazov",
+    author: "dostoevsky",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/4155MlQWD7L._SX324_BO1,204,203,200_.jpg"
+  },
+  %Library.Book{
+    name: "Crime and Punishment",
+    author: "dostoevsky",
+    image: "https://qph.cf2.quoracdn.net/main-qimg-9ff5d7256dc21668d2fcf2af51575439-lq"
+  }
 ]
 
 books
-|> Enum.each(fn book -> Library.Repo.insert(book) end)
+|> Enum.each(fn book -> Library.Repo.insert!(book) end)
