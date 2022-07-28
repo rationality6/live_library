21a5a48324fa47cfc171f8afaa70496507dd3882
Hyun Ahn <codepawn@icloud.com>
2022-07-28 18:08:16 +0900

    added seeds command

--- mix.exs
+++ mix.exs
@@ -63,7 +63,8 @@ defmodule Library.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.deploy": ["esbuild default --minify", "phx.digest"]
+      "assets.deploy": ["esbuild default --minify", "phx.digest"],
+      seeds: ["run priv/repo/seeds.exs"]
     ]
   end
 end
