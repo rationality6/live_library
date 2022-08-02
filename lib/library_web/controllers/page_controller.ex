fb5bff1b29b825f865a7cb05db3d58228ee42979
Hyun Ahn <codepawn@icloud.com>
2022-08-03 07:21:58 +0900

    Added books image (#2)
    * added seeds command
    
    * added seeds datas
    
    * added books image column
    
    * removed container class
    
    * removed container class
    
    * added books schema image
    
    * grid fix
    
    * clean up
    
    * clean up
    
    * added navigation
    
    * book to partial, added mt-2 on top

--- lib/library_web/controllers/page_controller.ex
+++ lib/library_web/controllers/page_controller.ex
@@ -5,12 +5,7 @@ defmodule LibraryWeb.PageController do
   alias Library.Repo
 
   def index(conn, _params) do
-    render(conn, "index.html")
-  end
-
-  def ecto_test(conn, _params) do
     all_books = Book |> Repo.all()
-    username = "foo"
-    render(conn, "ecto_test.html", username: username, all_books: all_books)
+    render(conn, "index.html", all_books: all_books)
   end
 end
